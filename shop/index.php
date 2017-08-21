877add6bbeea13e1f1d6bbc13f4eba060ebbdee1
ze25800000 <ze258100000@sina.com>
2017-08-22 05:33:05 +0800

    搭建Admin管理员登录页面Manager

--- shop/index.php
+++ shop/index.php
@@ -3,8 +3,12 @@ header('content-type:text/html;charset=utf8');
 //设置开发调试模式
 define('APP_DEBUG', true);
 //给系统资源文件请求路径设置常量
+//Home前台
 define('CSS_URL', '/shop/Home/Public/css');
 define('IMG_URL', '/shop/Home/Public/images');
 define('JS_URL', '/shop/Home/Public/js');
-
+//后台
+define('ADMIN_CSS_URL', '/shop/Admin/Public/css');
+define('ADMIN_IMG_URL', '/shop/Admin/Public/img');
+define('ADMIN_JS_URL', '/shop/Admin/Public/js');
 include '../ThinkPHP/ThinkPHP.php';
