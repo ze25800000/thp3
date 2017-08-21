fdad63ca8a6e46349691a9c32c9f4aba65c49470
ze25800000 <ze258100000@sina.com>
2017-08-21 23:08:02 +0800

    init

---
+++ shop/Home/Controller/UserController.class.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Home\Controller;
+
+use Think\Controller;
+
+class UserController extends Controller {
+    public function login() {
+        echo '用户登录系统';
+    }
+}
---
+++ shop/index.php
@@ -0,0 +1,3 @@
+<?php
+header('content-type:text/html;charset=utf8');
+include '../ThinkPHP/ThinkPHP.php';
