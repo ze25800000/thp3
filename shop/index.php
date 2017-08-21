82269615daa3859245fbba7ffb88bf8e3ef7d8a7
ze25800000 <ze258100000@sina.com>
2017-08-22 00:02:34 +0800

    引入静态资源文件js/css/img 引入时使用绝对路径 并且使用常量替换重复地址

--- shop/index.php
+++ shop/index.php
@@ -2,6 +2,9 @@
 header('content-type:text/html;charset=utf8');
 //设置开发调试模式
 define('APP_DEBUG', true);
-
+//给系统资源文件请求路径设置常量
+define('CSS_URL', '/shop/Home/Public/css');
+define('IMG_URL', '/shop/Home/Public/images');
+define('JS_URL', '/shop/Home/Public/js');
 
 include '../ThinkPHP/ThinkPHP.php';
