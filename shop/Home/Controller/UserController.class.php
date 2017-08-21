afa215385f5bccc56f1d5512706ad25930229232
ze25800000 <ze258100000@sina.com>
2017-08-21 23:17:32 +0800

    设置开发调试模式

--- shop/Home/Controller/UserController.class.php
+++ shop/Home/Controller/UserController.class.php
@@ -6,6 +6,6 @@ use Think\Controller;
 
 class UserController extends Controller {
     public function login() {
-        echo '用户登录系统';
+        $this->display();
     }
 }
--- shop/index.php
+++ shop/index.php
@@ -1,3 +1,7 @@
 <?php
 header('content-type:text/html;charset=utf8');
+//设置开发调试模式
+define('APP_DEBUG', true);
+
+
 include '../ThinkPHP/ThinkPHP.php';
