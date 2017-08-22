9be572c0476e7bfb93a4f24baecdcfcbacdace76
ze25800000 <ze258100000@sina.com>
2017-08-22 14:01:40 +0800

    品字页面搭建

---
+++ shop/Admin/Controller/GoodsController.class.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Admin\Controller;
+
+use Think\Controller;
+
+class GoodsController extends Controller {
+    public function showlist() {
+        $this->display();
+    }
+
+    public function tianjia() {
+        $this->display();
+    }
+
+    public function upd() {
+        $this->display();
+    }
+}
