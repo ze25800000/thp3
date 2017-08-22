2a3c6615b743214f222f7b57311cd7d6f8e8a80b
ze25800000 <ze258100000@sina.com>
2017-08-22 10:09:56 +0800

    搭建admin

---
+++ shop/Admin/Controller/GoodsController.class.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Admin\Controller;
+use Think\Controller;
+
+class GoodsController extends Controller {
+    public function showlist() {
+        $this->display();
+    }
+
+    public function tianjia() {
+        $this->display();
+    }
+
+    public function udp() {
+        $this->display();
+    }
+}
--- shop/index.php
+++ shop/index.php
@@ -6,5 +6,8 @@ define('APP_DEBUG', true);
 define('CSS_URL', '/shop/Home/Public/css');
 define('IMG_URL', '/shop/Home/Public/images');
 define('JS_URL', '/shop/Home/Public/js');
-
+//admin管理员
+define('ADMIN_CSS_URL', '/shop/Admin/Public/css');
+define('ADMIN_IMG_URL', '/shop/Admin/Public/img');
+define('ADMIN_JS_URL', '/shop/Admin/Public/js');
 include '../ThinkPHP/ThinkPHP.php';
