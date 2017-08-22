0e7e2c36ea7f2c231ac784e0f0d5cac63727ea7c
ze25800000 <ze258100000@sina.com>
2017-08-22 14:46:42 +0800

    设置请求的默认分组

--- shop/Common/Conf/config.php
+++ shop/Common/Conf/config.php
@@ -1,5 +1,9 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
-    'SHOW_PAGE_TRACE'=>true
+    //'配置项'=>'配置值'
+    'SHOW_PAGE_TRACE' => true,
+    //设置请求的默认分组
+    'DEFAULT_MODULE'  => 'Home',
+    //设置一个对比的分组列表
+    'MODULE_ALLOW_LIST'=>['Admin','Home']
 );
