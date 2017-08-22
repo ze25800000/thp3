ca87563a59444fd4a604053a7d6be6e54a2454e0
ze25800000 <ze258100000@sina.com>
2017-08-22 15:25:57 +0800

    使用smarty模板引擎

--- shop/Common/Conf/config.php
+++ shop/Common/Conf/config.php
@@ -1,9 +1,16 @@
 <?php
 return array(
     //'配置项'=>'配置值'
-    'SHOW_PAGE_TRACE' => true,
+    'SHOW_PAGE_TRACE'    => true,
     //设置请求的默认分组
-    'DEFAULT_MODULE'  => 'Home',
+    'DEFAULT_MODULE'     => 'Home',
     //设置一个对比的分组列表
-    'MODULE_ALLOW_LIST'=>['Admin','Home']
+    'MODULE_ALLOW_LIST'  => ['Admin', 'Home'],
+    //开启smarty模板引擎
+    'TMPL_ENGINE_TYPE'   => 'Smarty',
+    //smarty相关配置
+//    'TMPL_ENGINE_CONFIG' => [
+//        'left_delimiter'=>'<%%%',
+//        'right_delimiter'=>'%%%>',
+//    ]
 );
