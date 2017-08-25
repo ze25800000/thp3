fb98ce7fd5195a0479a8cfc4e23c7cd42d7fae80
ze25800000 <ze258100000@sina.com>
2017-08-25 16:32:51 +0800

    实现用户注册

--- shop/Home/Controller/UserController.class.php
+++ shop/Home/Controller/UserController.class.php
@@ -8,4 +8,15 @@ class UserController extends Controller {
     public function login() {
         $this->display();
     }
+
+    public function register() {
+        $user = D('User');
+        if (!empty($_POST)) {
+            $_POST['user_hobby'] = implode(',', $_POST['user_hobby']);
+            $z = $user->add($_POST);
+            echo $z;
+        } else {
+            $this->display();
+        }
+    }
 }
