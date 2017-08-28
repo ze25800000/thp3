0456620aa8c5ecd0ddc7167fefb5a1ad263afb96
ze25800000 <ze258100000@sina.com>
2017-08-29 05:53:09 +0800

    验证码校验

--- shop/Admin/Controller/ManageController.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Admin\Controller;
-use Think\Controller;
-
-class ManageController extends Controller {
-    public function login() {
-        $this->display();
-    }
-}
--- shop/Admin/Controller/ManagerController.class.php
+++ shop/Admin/Controller/ManagerController.class.php
@@ -3,9 +3,30 @@
 namespace Admin\Controller;
 
 use Think\Controller;
+use Think\Verify;
 
 class ManagerController extends Controller {
     public function login() {
+        if (!empty($_POST)) {
+            $vry = new Verify();
+            if ($vry->check($_POST['captcha'])) {
+                echo '验证码正确';
+            } else {
+                echo '错误';
+            }
+        }
         $this->display();
+    }
+
+    public function verifyImg() {
+        $cfg  = [
+            'imageH'   => 35,
+            'imageW'   => 90,
+            'fontSize' => 13,
+            'length'   => 4,
+            'fontttf'  => '2.ttf',
+        ];
+        $very = new Verify($cfg);
+        $very->entry();
     }
 }
