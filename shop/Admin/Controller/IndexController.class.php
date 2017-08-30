71176d8adb8c57c375a0e3e4a757983f68ca0bd9
ze25800000 <ze258100000@sina.com>
2017-08-30 08:07:42 +0800

    权限控制

--- shop/Admin/Controller/IndexController.class.php
+++ shop/Admin/Controller/IndexController.class.php
@@ -14,6 +14,21 @@ class IndexController extends Controller {
     }
 
     public function left() {
+        //用户id
+        $admin_id = session('admin_id');
+        //根据id获得用户信息
+        $admin_info = D('Manager')->find($admin_id);
+        //角色id
+        $role_id = $admin_info['mg_role_id'];
+        //获得角色信息
+        $role_info = D('Role')->find($role_id);
+        //权限的ids信息
+        $auth_ids = $role_info['role_auth_ids'];
+        //获取全部权限数据
+        $auth_infoA = D('Auth')->where("auth_level=0 and auth_id in ($auth_ids)")->select();//顶级权限
+        $auth_infoB = D('Auth')->where("auth_level=1 and auth_id in ($auth_ids)")->select();//次顶级
+        $this->assign('auth_infoA', $auth_infoA);
+        $this->assign('auth_infoB', $auth_infoB);
         $this->display();
     }
 
